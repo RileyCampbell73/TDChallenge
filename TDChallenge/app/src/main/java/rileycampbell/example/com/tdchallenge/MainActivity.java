8722e99d71737a36bcbf939ff86836aedb404d8f
Riley Campbell <riley.campbell73@gmail.com>
2014-11-09 14:50:15 -0500

    Some little additions. Refined location to be more specific. Now prompts user when 1 place or 0 places are found.

--- TDChallenge/app/src/main/java/rileycampbell/example/com/tdchallenge/MainActivity.java
+++ TDChallenge/app/src/main/java/rileycampbell/example/com/tdchallenge/MainActivity.java
@@ -1,6 +1,9 @@
 package rileycampbell.example.com.tdchallenge;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -39,9 +42,6 @@ final String API_KEY = "AIzaSyDyQ-faomCpZDP_TIMqJm0OOBfZm12vvlw";
             // Here you can ask the user to try again, using return; for that
             Toast.makeText(getBaseContext(), "Your location is not available, please try again.", Toast.LENGTH_SHORT).show();
             return;
-
-            // Or you can continue without getting the location, remove the return; above and uncomment the line given below
-            // address = "Location not available";
         } else {
 
             // Getting location co-ordinates
@@ -54,20 +54,19 @@ final String API_KEY = "AIzaSyDyQ-faomCpZDP_TIMqJm0OOBfZm12vvlw";
             //Grabs all the places near user
             new GetPlaces().execute();
 
+
             }
         }
 
 
 
     private class GetPlaces extends AsyncTask<Void, Void,  ArrayList<Place>>{
+        private ProgressDialog dialog;
 
-        protected String getJSON(String url) {
-            return getUrlContents(url);
-        }
-
-        private String getUrlContents(String theUrl) {
+        private String getJSON(String theUrl) {
             StringBuilder content = new StringBuilder();
             try {
+                //making the request to google
                 URL url = new URL(theUrl);
                 URLConnection urlConnection = url.openConnection();
                 BufferedReader bufferedReader = new BufferedReader(
@@ -83,49 +82,117 @@ final String API_KEY = "AIzaSyDyQ-faomCpZDP_TIMqJm0OOBfZm12vvlw";
             }
             return content.toString();
         }
-
+            //before the call to google
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                dialog = new ProgressDialog(MainActivity.this);
+                dialog.setCancelable(false);
+                dialog.setMessage("Loading..");
+                dialog.isIndeterminate();
+                dialog.show();
+            }
+            //after the call to google
             @Override
             protected void onPostExecute(ArrayList<Place> result) {
-                ListView lv = (ListView)findViewById(R.id.listViewPlaces);
-                ArrayList<String> PlaceNames = new ArrayList<String>();
-                for (Place s : result){
-                    PlaceNames.add(s.getName());
+                if (dialog.isShowing()) {
+                    dialog.dismiss();
                 }
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                        MainActivity.this,
-                        android.R.layout.simple_list_item_1,
-                        PlaceNames );
-                lv.setAdapter(arrayAdapter);
+                //TEST CODE for testing 1 result
+                Place temp = result.get(0);
+                result.clear();
+               //result.add(temp);
+
+                if (result.size() == 1){//ONLY 1 RESULT
+                    // 1. Instantiate an AlertDialog.Builder with its constructor
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                    // 2. Chain together various setter methods to set the dialog characteristics
+                    builder.setMessage("Are you currently at " + result.get(0).getName() + "? \n\nIf you select no the app will attempt to find the store your currently at again.")
+                            .setTitle("Are You Here?");
+                    builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User clicked yes button
+                        }
+                    });
+                    builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+                    // 3. Get the AlertDialog from create()
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else if (result.size() > 1){//MORE THEN 1 RESULT
+                    ListView lv = (ListView)findViewById(R.id.listViewPlaces);
+                    ArrayList<String> PlaceNames = new ArrayList<String>();
+                    for (Place s : result){
+                        PlaceNames.add(s.getName());
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                            MainActivity.this,
+                            android.R.layout.simple_list_item_1,
+                            PlaceNames );
+                    lv.setAdapter(arrayAdapter);
+                }
+                else{//NO RESULTS
+                    // 1. Instantiate an AlertDialog.Builder with its constructor
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                    // 2. Chain together various setter methods to set the dialog characteristics
+                    builder.setMessage("No store locations were found near your location. Would you like to search again?")
+                            .setTitle("Where are you?");
+                    builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User clicked yes button
+                        }
+                    });
+                    builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+                    // 3. Get the AlertDialog from create()
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+
+
+
             }
+            //this will call google and get places at the users current latlong
             @Override
             protected ArrayList<Place> doInBackground(Void... params){
-
+                //build the url to be sent to get results
                 StringBuilder urlString = new StringBuilder(
                         "https://maps.googleapis.com/maps/api/place/search/json?");
+                //may be able to limit this further. this is kinda overkill
                 urlString.append("types=restaurant|cafe|bakery|bar|beauty_salon|bicycle_store|book_store|bowling_alley|clothing_store|convenience_store|" +
                         "department_store|electronics_store|pet_store|shoe_store|shopping_mall|store|furniture_store|gas_station|grocery_or_supermarket|" +
                         "hardware_store|home_goods_store|jewelry_store|liquor_store");//MAYBE REMOVE: Shopping Mall, Bar
+               // urlString.append("&rankby=distance");
                 urlString.append("&location=");
                 urlString.append(Double.toString(latitude));
                 urlString.append(",");
                 urlString.append(Double.toString(longitude));
-                urlString.append("&radius=1000");
-                // urlString.append("&types="+place);
+                urlString.append("&radius=500");//meters
                 urlString.append("&sensor=false&key=" + "AIzaSyAsb8XP5659RBOaQSYK5e71Ta2Z0CQ_5Q4"); //THIS USES SERVER KEY
-
+                //CANNOT HAVE 'radius' AND 'rankby' in the same statment. So will keep radius small
+                //The goal it to have very few places show up.
+                //if only 1 result is there then ask them if there currently their.
+                //if multiple, then show list
                 try {
+                    //sends the request
                     String json = getJSON(urlString.toString());
                     System.out.println(json);
                     JSONObject object = new JSONObject(json);
                     JSONArray array = object.getJSONArray("results");
-
+                    //takes the JSON array and puts into an array of Places
                     for (int i = 0; i < array.length(); i++) {
-
                         try {
-
                             Place place = Place.jsonToPontoReferencia((JSONObject) array.get(i));
                             Log.v("Places Services ", "" + place);
-
                             arrayList.add(place);
                         } catch (Exception e) {
                             //ERROR HANDLE IT
@@ -137,7 +204,7 @@ final String API_KEY = "AIzaSyDyQ-faomCpZDP_TIMqJm0OOBfZm12vvlw";
                 }
                 return arrayList;
             }
-}
+    }
         @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
