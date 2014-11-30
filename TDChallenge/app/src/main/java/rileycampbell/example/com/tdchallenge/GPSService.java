5d34183ad1305b26969ed695b2197393ba3f2ba3
Riley Campbell <riley.campbell73@gmail.com>
2014-11-29 21:33:28 -0500

    Bug Fix - app not getting new location. SHOULD be fixed now

--- TDChallenge/app/src/main/java/rileycampbell/example/com/tdchallenge/GPSService.java
+++ TDChallenge/app/src/main/java/rileycampbell/example/com/tdchallenge/GPSService.java
@@ -40,9 +40,9 @@ public class GPSService extends Service implements LocationListener {
     double mLongitude;
 
     // Minimum time fluctuation for next update (in milliseconds)
-    private static final long TIME = 30000;
+    private static final long TIME = 0;
     // Minimum distance fluctuation for next update (in meters)
-    private static final long DISTANCE = 20;
+    private static final long DISTANCE = 0;
 
     // Declaring a Location Manager
     protected LocationManager mLocationManager;
@@ -119,65 +119,6 @@ public class GPSService extends Service implements LocationListener {
         isLocationAvailable = false;
         return null;
     }
-
-    /**
-     * Gives you complete address of the location
-     *
-     * @return complete address in String
-     */
-    public String getLocationAddress() {
-
-        if (isLocationAvailable) {
-
-            Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
-            // Get the current location from the input parameter list
-            // Create a list to contain the result address
-            List<Address> addresses = null;
-            try {
-				/*
-				 * Return 1 address.
-				 */
-                addresses = geocoder.getFromLocation(mLatitude, mLongitude, 1);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-                return ("IO Exception trying to get address:" + e1);
-            } catch (IllegalArgumentException e2) {
-                // Error message to post in the log
-                String errorString = "Illegal arguments "
-                        + Double.toString(mLatitude) + " , "
-                        + Double.toString(mLongitude)
-                        + " passed to address service";
-                e2.printStackTrace();
-                return errorString;
-            }
-            // If the reverse geocode returned an address
-            if (addresses != null && addresses.size() > 0) {
-                // Get the first address
-                Address address = addresses.get(0);
-				/*
-				 * Format the first line of address (if available), city, and
-				 * country name.
-				 */
-                String addressText = String.format(
-                        "%s, %s, %s",
-                        // If there's a street address, add it
-                        address.getMaxAddressLineIndex() > 0 ? address
-                                .getAddressLine(0) : "",
-                        // Locality is usually a city
-                        address.getLocality(),
-                        // The country of the address
-                        address.getCountryName());
-                // Return the text
-                return addressText;
-            } else {
-                return "No address found by the service: Note to the developers, If no address is found by google itself, there is nothing you can do about it.";
-            }
-        } else {
-            return "Location Not available";
-        }
-
-    }
-
 
 
     /**
